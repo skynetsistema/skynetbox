031f7e48c7429210704ab9590bb4ec60a9c6dcd3
Alex Sodre <skynetsistema2020@gmail.com>
2020-03-31 09:41:38 -0300

    communication with the database

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const routes = express.Router();
 
-routes.get('/teste', (req, res) => {
-  return res.send('Hello SkynetBox');
-})
+const BoxController = require('./controllers/BoxController');
+
+routes.post('/boxes', BoxController.store);
 
 module.exports = routes;
